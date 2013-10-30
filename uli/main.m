6614dad5a1fad07385953baa12a49a9de3f4ef6b
yzlin <yzlin1985@gmail.com>
2013-10-30 23:18:10 +0800

    Initial Commit

---
+++ uli/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  uli
+//
+//  Created by Ethan Lin on 10/30/13.
+//  Copyright (c) 2013 github.com/yzlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
