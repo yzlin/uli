1e579a629e82c2f6747739792919693044fc0858
yzlin <yzlin1985@gmail.com>
2013-11-01 22:49:07 +0800

    Removed unused window.

--- uli/ULIAppDelegate.m
+++ uli/ULIAppDelegate.m
@@ -10,21 +10,10 @@
 
 #import "ULIAppDelegate.h"
 
-@interface ULIAppDelegate ()
-
-@property (nonatomic, strong) NSWindow *window;
-
-@end
-
 @implementation ULIAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-    self.window = [[NSWindow alloc] initWithContentRect:NSMakeRect(0, 0, 200, 200) styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO];
-    [_window cascadeTopLeftFromPoint:NSMakePoint(20, 20)];
-    _window.title = @"test";
-    [_window makeKeyAndOrderFront:nil];
-
     [[ULIDrawRect defaultInstance] listen];
     NSLog(@"server launched");
 }
